be19b054a4865d585d1847c0d931182628c3409b
Thami Maseko <tkmaseko94@gmail.com>
2026-04-07 11:51:14 +0200

    added Approve user and also added login a new user test

--- src/test/java/Basic/UserRegistration.java
+++ src/test/java/Basic/UserRegistration.java
@@ -6,6 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.github.javafaker.Faker;
 
+
+
 public class UserRegistration {
 
     static String authToken;
@@ -17,8 +19,8 @@ public class UserRegistration {
     public void adminLoginTest() {
         String apiPath = "/APIDEV/login";
         String payload = "{\n" +
-                "    \"email\": \"tk@gmail.com\",\n" +
-                "    \"password\": \"Agric123@\"\n" +
+                "    \"email\": \"spare@admin.com\",\n" +
+                "    \"password\": \"@12345678\"\n" +
                 "}";
 
         Response response = RestAssured.given()
@@ -42,10 +44,10 @@ public class UserRegistration {
                 "    \"firstName\": \"dsfdsa\",\n" +
                 "    \"lastName\": \"sdfdsaf\",\n" +
                 "    \"email\": \"%s\",\n" +
-                "    \"password\": \"@a12345678\",\n" +
-                "    \"confirmPassword\": \"@a12345678\",\n" +
+                "    \"password\": \"Agric123@\",\n" +
+                "    \"confirmPassword\": \"Agric123@\",\n" +
                 "    \"phone\": \"\",\n" +
-                "    \"groupId\": \"5328c91e-fc40-11f0-8e00-5000e6331276\"\n" +
+                "    \"groupId\": \"1f49eb00-ff0d-476c-8ecc-6ff2fb53a35c\"\n" +
                 "}", registeredEmail);
 
         Response response = RestAssured.given()
@@ -57,9 +59,50 @@ public class UserRegistration {
                 .post().prettyPeek();
 
         int actualStatusCode = response.getStatusCode();
-        Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
+        Assert.assertEquals(actualStatusCode, 201, "Status code should be 201");
         userId = response.jsonPath().getString("data.id");
     }
+
+@Test(priority = 3)
+public void approveUserRegistration(){
+
+    String apiPath = "/APIDEV/admin/users/"+ UserRegistration.userId+"/approve";
+
+    Response response = RestAssured.given()
+            .baseUri(UserRegistration.baseURL)
+            .basePath(apiPath)
+            .header("Content-Type", "application/json")
+            .header("Authorization", "Bearer " + UserRegistration.authToken)
+            .log().all()
+            .put().prettyPeek();
+
+    int actualStatusCode = response.getStatusCode();
+    Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
+
+}
+@Test(priority = 4)
+public void userLoginTest() {
+
+    String apiPath = "/APIDEV/login";
+    String payload = String.format( "{\n" +
+            "    \"email\": \"%s\",\n" +
+            "    \"password\": \"Agric123@\"\n" +
+            "}", UserRegistration.registeredEmail);
+
+    Response response = RestAssured.given()
+            .baseUri(UserRegistration.baseURL)
+            .basePath(apiPath)
+            .header("Content-Type", "application/json")
+            .body(payload)
+            .log().all()
+            .post().prettyPeek();
+
+    int actualStatusCode = response.getStatusCode();
+    Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
+
+}
+
+
 }
 
 
